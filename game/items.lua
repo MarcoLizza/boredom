72abfc1bcb05eba78cf750eda38c191ef6eccf6b
Marco Lizza <marco.lizza@gmail.com>
2016-01-17 22:25:30 +0100

    Scribbling...

--- game/items.lua
+++ game/items.lua
@@ -1,10 +1,10 @@
 local items = {
   list = {
     {
-      tiles = { },
-      description = '',
+      tiles = { { x = 1, y = 4} },
+      description = 'Stereo player',
       features = {
-        fatigue = 1,
+        fatigue = 0,
         time = 2,
         fun = 3,
         health = 0,
@@ -14,9 +14,17 @@ local items = {
   }
 }
 
-
-function items.collide_with(x, y)
+function items:collide_with(x, y)
+  local set = { }
+  for _, item in ipairs(self.list)
+    for _, tile in ipairs(item.tiles)
+      if x == tile.x and y == tile.y then
+        set[#set + 1] = item
+        break
+      end
+    end
+  end
+  return set
 end
-
 
 return items
